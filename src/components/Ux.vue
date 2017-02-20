d352bbc0c7c9d83da29b5686a9f1e0aabcc9a6b5
Benjamin Beguin <beguin.ben@gmail.com>
2017-02-20 19:45:47 +0100

    ux componnent

--- src/components/Ux.vue
+++ src/components/Ux.vue
@@ -1,23 +1,49 @@
 <template>
   <div class="page page-ux">
-    <div class="wrapper rose">
-      <br><h1>COMMON</h1>
+
+
+    <div class="wrapper green">
+      <br>
+      <br>
+      <h1>COMMON</h1>
       <p class="small-text">.small-text</p>
       <p class="text">.text</p>
       <p class="text-ancors color-colored">.text-ancors</p>
 
       <span class="project-bar background-colored"></span>
 
-      <br><h1>SLIDER</h1>
+      <br>
+      <br>
+      <h1>SLIDER</h1>
       <h1 class="slider-title color-colored ">.slider-title</h1>
       <p class="slider-subtitle">.slider-subtitle</p>
 
-      <br><h1>PROJECT CASE STUDY</h1>
+      <br>
+      <br>
+      <h1>PROJECT CASE STUDY</h1>
       <h1 class="project-title color-gray">.project-title</h1>
       <h1 class="project-title__part color-gray">.project-title__part</h1>
       <p class="project-subtitle color-colored">.project-subtitle</p>
+
+
+      <br>
+      <br>
+      <br>
+      <h1>TEST</h1>
+
+      <br><br>
+
+      <h1 class="project-title color-gray">VOICES OF LEGENDS</h1>
+      <p class="project-subtitle color-colored">ART DIRECTION - STORYBOARDS - MOTION DESIGN - SOUND DESIGN</p>
+      <p class="text">Voices of legends is a mini-game based on the famous video-game League of Legends and developped by a french streamer Domingo. The goal is to find 20 champions’s voices in the shortest amount of time. The players are rewarded thanks to xp and chest with bonus on the domingo.tv website. My job was to create the teaser.</p>
+      <p class="text-ancors color-colored">Watch ancors</p>
+
+
     </div>
   </div>
+
+
+
 </template>
 
 <script>
