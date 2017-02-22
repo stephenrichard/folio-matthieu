5db7e40d9d073c76f7fe7c7a6b7e11da55710632
Benjamin Beguin <beguin.ben@gmail.com>
2017-02-22 22:42:54 +0100

    design template

--- src/components/Ux.vue
+++ src/components/Ux.vue
@@ -8,7 +8,7 @@
       <h1>COMMON</h1>
       <p class="small-text">.small-text</p>
       <p class="text">.text</p>
-      <p class="text-ancors color-colored">.text-ancors</p>
+      <p class="text-ancors color-colored before-colored">.text-ancors</p>
 
       <span class="project-bar background-colored"></span>
 
@@ -22,7 +22,7 @@
       <br>
       <h1>PROJECT CASE STUDY</h1>
       <h1 class="project-title color-gray">.project-title</h1>
-      <h1 class="project-title__part color-gray">.project-title__part</h1>
+      <h1 class="project-part__title color-gray">.project-part__title</h1>
       <p class="project-subtitle color-colored">.project-subtitle</p>
 
 
@@ -32,13 +32,62 @@
       <h1>TEST</h1>
 
       <br><br>
+    </div>
 
-      <h1 class="project-title color-gray">VOICES OF LEGENDS</h1>
-      <p class="project-subtitle color-colored">ART DIRECTION - STORYBOARDS - MOTION DESIGN - SOUND DESIGN</p>
-      <p class="text">Voices of legends is a mini-game based on the famous video-game League of Legends and developped by a french streamer Domingo. The goal is to find 20 champions’s voices in the shortest amount of time. The players are rewarded thanks to xp and chest with bonus on the domingo.tv website. My job was to create the teaser.</p>
-      <p class="text-ancors color-colored">Watch ancors</p>
+    <div class="rose">
 
+      <section class=" wrapper project-header rose">
+        <h1 class="project-title color-gray">VOICES OF LEGENDS</h1>
+        <p class="project-subtitle color-colored">ART DIRECTION - STORYBOARDS - MOTION DESIGN - SOUND DESIGN</p>
+        <div class="project-header__content">
+          <p class="text">Voices of legends is a mini-game based on the famous video-game League of Legends and developped by a french streamer Domingo. The goal is to find 20 champions’s voices in the shortest amount of time. The players are rewarded thanks to xp and chest with bonus on the domingo.tv website. My job was to create the teaser.</p>
+          </div>
+        <p class="text-ancors color-colored before-colored">Watch ancors</p>
+      </section>
 
+      <section class=" wrapper project-part">
+          <div class="project-part__header">
+              <h2 class="project-part__title color-gray">Storyboard and modeling</h2>
+              <p class="text project-part__intro">The next step was to create the scenario and a draft storyboard. After I jumped into Cinema 4D to build the models needed and challenge the storyboard. It also allows me to try severals looks and point of view locking cameras for later when something looked nice.</p>
+          </div>
+      </section>
+
+      <section class="project-part__bannerBG">
+        <div class="project-part__bannerBackground" style="background-image: url('static/img/baniere.png');"></div>
+      </section>
+
+      <section class="wrapper project-part moodboard ">
+          <div class="project-part__header">
+            <h2 class="project-part__title color-gray">THE MOODBOARD</h2>
+              <p class="text project-part__intro">At the time I was quite interested in retro effects and old 8 bit videogames so I built a moodboard based on this. My main references were <strong>FITC // Toronto 2016 Teaser</strong> by Giant Ant, <strong>DDD + OFFF Italy 2016 Main Titles</strong>  by Nerdo and <strong>Adobe Make It Titles</strong> by Never Sit Still. I also did a wink to league of legends adding Poros, sweet little creatures.</p>
+          </div>
+          <div class="project-part__picture">
+            <img src="../assets/img/moodboard.png" alt="">
+          </div>
+
+      </section>
+
+      <section class=" wrapper project-part">
+          <div class="project-part__header">
+              <h2 class="project-part__title color-gray">DESIGNBOARD</h2>
+              <p class="text project-part__intro">Simultaneously with the modeling part, I started my designboard and compositing. I wanted to create a mysterious atmosphere and build tension until the drop where the 8-bit menu shows up. I imagined 3 phases: [1] Screen saver, [2] Terminal and [3] 8-bit menu in game.</p>
+          </div>
+
+          <div class="project-part__bannerIMG ">
+            <img src="static/img/designboard.png" alt="">
+          </div>
+      </section>
+
+      <section class=" wrapper project-part">
+          <div class="project-part__header">
+              <h2 class="project-part__title color-gray">ANIMATION & SOUND DESIGN</h2>
+              <p class="text project-part__intro">Once the styleframes felt good, I prepared my assets inside Photoshop and started rendering in Cinema 4D. Animate the titles was quite time-consuming but I have been rewarded by the results and kept going. The audio was the trickiest… To bring life to my animation, the audio was essential but it was hard to find the exact sound which fit both movements and atmosphere. </p>
+          </div>
+
+          <div class="project-part__vimeo">
+            <iframe src="https://player.vimeo.com/video/199319292" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+      </section>
     </div>
   </div>
 
