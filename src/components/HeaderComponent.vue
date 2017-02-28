f6ff66755ba9b65fea054301500102fc0d75e8a4
stephen-richard <stephen.richard@hetic.net>
2017-02-28 10:55:32 +0100

    Back button

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="header">
-    <header>
+    <header :class="getCurrentProject.color">
       <router-link to="/" class="logo">
       	<img src="../assets/logo@2x.png" alt="Matthieu Tourdes motion designer">
       	<h1 class="color-gray small-text">Matthieu Tourdes</h1>
       </router-link>
       <div class="more-back-links">
         <router-link v-if="getPage === 'home'" to="/about" class="small-text">More about me</router-link>
-        <router-link v-show="getPage != 'home'" to="/" class="back-link">Back to menu</router-link>
+        <router-link v-show="getPage != 'home'" to="/" class="back-link small-text before-colored project-bar">Back to menu</router-link>
       </div>
     </header>
   </div>
@@ -20,13 +20,15 @@
     name: 'headerComponent',
     computed: {
       ...mapGetters([
-        'getPage'
+        'getPage',
+        'getCurrentProject'
       ])
     }
   }
 </script>
 
 <style lang="sass">
+  @import '../stylesheets/common/_color'
 
   .header
     position: absolute
@@ -40,7 +42,6 @@
       a
         display: inline-block
         text-decoration: none
-        font-size: 12px
         letter-spacing: 1.7px
         line-height: 16px
         text-transform: uppercase
@@ -69,7 +70,28 @@
         font-weight: 600
 
         .back-link
+          position: relative
+          padding-top: 10px
           font-weight: 800
+          font-size: 11px
+          color: $dark
+          text-align: center
+          transition: color ease .2s
 
+          &:hover
+            color: white
+
+            &:before
+              height: 35px
+
+          &:before
+            position: absolute
+            content: ''
+            top: 0
+            left: 0
+            right: 0
+            height: 6px
+            z-index: -1
+            transition: height .3s ease
 
 </style>
